23cf0341a81a7fa6c17b33d8df76522abb779921
Devin Rattray <devinrattray@Devins-MacBook-Pro-2.local>
2022-11-23 17:44:34 -0800

    initial commit

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // Importing libraries
 import { ThirdwebSDK } from '@thirdweb-dev/sdk';
-import { createWriteStream } from 'fs';
+import { createWriteStream, readFileSync } from 'fs';
 import { Alchemy, Network } from 'alchemy-sdk';
 import { BigNumberish, constants } from 'ethers';
 
@@ -14,14 +14,17 @@ const alchemy = new Alchemy(alchemySettings);
 // Instantiate thirdweb SDK
 const sdk = new ThirdwebSDK('optimism');
 
-const erc: 'erc721' | 'erc721' = 'erc721'; // Choose erc721 or erc1155
+const erc: 'erc721' | 'erc1155' = 'erc721'; // Choose erc721 or erc1155
 const contractAddress = '0x3e7e82fB003cAa50930F288c165f8E895f02117B'; // Replace with your contract address
 const tokenId = '0'; // Replace with the token ID to check the balance of (in case of ERC1155, otherwise ignore)
 
-const getBalances = async (erc: "erc721" | "erc721") => {
-  const contract = await sdk.getContract(contractAddress);
+const getBalances = async (erc: "erc721" | "erc1155") => {
+  const contract = await sdk.getContractFromAbi(
+    contractAddress, 
+    JSON.parse(readFileSync("myABI.json", "utf8"))
+  );
   let addresses: string[] = [];
-  if (erc === 'erc721') {
+  if (erc === 'erc1155') {
     addresses = (
       await alchemy.nft.getOwnersForNft(contractAddress, tokenId)
     ).owners.filter((addr) => addr !== constants.AddressZero);
@@ -38,7 +41,7 @@ const getBalances = async (erc: "erc721" | "erc721") => {
     for (let i = 0; i < addresses.length; i++) {
       const address = addresses[i];
       let balanceOf: BigNumberish = 0;
-      if (erc === 'erc721') {
+      if (erc === 'erc1155') {
         balanceOf = await contract.erc1155.balanceOf(address, tokenId);
       } else if (erc === 'erc721') {
         balanceOf = await contract.erc721.balanceOf(address);
